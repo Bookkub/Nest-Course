8fb25bd04a6f457f15775d1ba9338c4ff3b26661
Kittiphot-Book <kittiphot@playtorium.co.th>
2025-05-25 12:53:37 +0700

    feat: implement user management with DTOs and service methods for CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,37 +7,46 @@ import {
   Post,
   Put,
   Query,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get() // GET /users
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
-  }
-
-  @Get('interns') // GET /users/interns
-  findAllInterns() {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id') // GET /users/:id
-  findById(@Param('id') id: string) {
-    return { id };
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body(ValidationPipe)
+    createUserDto: CreateUserDto,
+  ) {
+    return this.usersService.create(createUserDto);
   }
 
   @Put(':id') // PUT /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe)
+    updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return { id };
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
